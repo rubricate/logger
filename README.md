f1dba777e8b4bb35a7d2686757f5e8e61fcb9fd5
Estefanio N Santos <estefanions@gmail.com>
2022-10-02 14:08:35 -0300

    STYLE - Adapted for 'Rubricate' components

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Logger
 PHP Component for general purpose logging
+
+```
+$ composer require rubricate/logger
+```
+
+- Documentation is at https://rubricate.github.io/components/logger
+
--- Rubricate/Logger/AbstractLogger.php
+++ Rubricate/Logger/AbstractLogger.php
@@ -1,137 +1,150 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+declare(strict_types=1);
 
-/**
- * Description of Log
- *
- * @author Andre
- */
-class Log {
+namespace Rubricate\Logger;
 
-    /**
-     * Enable debug mode as default
-     * @var type 
-     */
-    public static $enabled = DOMINIO == 'dev';
+ini_set ('xdebug.overload_var_dump', 0);
 
-    /**
-     * Grava informações no Log com label INFO
-     */
-    public static function info() {
-        if (self::$enabled || defined('ENABLE_LOG')) {
-            foreach (func_get_args() as $info) {
-                self::setLog("INFO", $info);
-            }
+use Rubricate\Logger\ConstLogger as C;
+
+abstract class AbstractLogger
+{
+    protected static $ex = '.txt';
+    abstract protected static function getDirFullPath();
+    abstract protected static function getPrefixToFile();
+
+    public static function info()
+    {
+        foreach (func_get_args() as $info) {
+            self::set(C::TYPE_INFO, $info);
         }
     }
 
-    /**
-     * Grava informações no Log com label ERROR
-     */
-    public static function error() {
+    public static function error()
+    {
         foreach (func_get_args() as $error) {
-            self::setLog("ERROR", $error);
+            self::set(C::TYPE_ERROR, $error);
         }
     }
 
-    /**
-     * Grava informações no Log com label ERROR
-     * @param type $error
-     */
-    public static function debug() {
+    public static function debug()
+    {
         foreach (func_get_args() as $debug) {
-            self::setLog("DEBUG", $debug);
+            self::set(C::TYPE_DEBUG, $debug);
         }
     }
 
-    /**
-     * Grava informações no Log com label ERROR
-     * @param type $error
-     */
-    public static function trace() {
+    public static function trace()
+    {
+        $type = null;
+        $data = '';
+
         foreach (func_get_args() as $log) {
-            if (is_array($log)) {
-                $str = date('d/m/Y H:i:s') . ": ";
-                $str .= print_r($log, true);
-            } else if (is_object($log)) {
-                if (get_class($log) == "Exception" || is_subclass_of($log, "Exception")) {
-                    $str = date('d/m/Y H:i:s') . ": {$log->getTraceAsString()}";
-                } else {
-                    $str = date('d/m/Y H:i:s') . ": ";
-                    $str .= print_r($log, true);
-                }
-            } else {
-                $str = date('d/m/Y H:i:s') . ": {$log}\n";
-            }
-            self::setLog("TRACE", $str);
+
+            $data = self::getDataToWrite($type, $log);
+            self::set(C::TYPE_TRACE, $data);
         }
+
         $trace = [];
+
         foreach (debug_backtrace() as $v) {
+
             foreach ($v['args'] as &$arg) {
                 if (is_object($arg)) {
                     $arg = '(Object)';
                 }
             }
+
             array_push($trace, array_filter($v, function($key) {
-                        return $key != 'object';
-                    }, ARRAY_FILTER_USE_KEY));
+                return $key != 'object';
+            }, ARRAY_FILTER_USE_KEY));
         }
-        $str .= "Trace: " . print_r($trace, true);
-        self::setLog("TRACE", $str);
+
+        $data .= "Trace: " . print_r($trace, true);
+        self::set(C::TYPE_TRACE, $data);
     }
 
-    /**
-     * Grava dados no log
-     */
-    private static function setLog($tipo, $log) {
-        $logdir = __DIR__ . "/../logs/";
-        if ($tipo != 'TRACE') {
-            if (is_array($log)) {
-                $str = date('d/m/Y H:i:s') . " {$tipo}: ";
-                $str .= print_r($log, true);
-            } else if (is_object($log)) {
-                if (get_class($log) == "Exception" || is_subclass_of($log, "Exception")) {
-                    $str = date('d/m/Y H:i:s') . " {$tipo}: {$log->getTraceAsString()}";
-                } else {
-                    $str = date('d/m/Y H:i:s') . " {$tipo}: ";
-                    $str .= print_r($log, true);
-                }
-            } else {
-                $str = date('d/m/Y H:i:s') . " {$tipo}: {$log}\n";
-            }
-            $file = __DIR__ . "/../logs/" . DOMINIO . "_ws_" . date('Y-m-d') . ".txt";
-        } else {
-            $file = __DIR__ . "/../logs/" . DOMINIO . "_ws_trace_" . date('Y-m-d') . ".txt";
-            $str = $log;
+    private static function set($type, $log)
+    {
+        $logdir = static::getDirFullPath();
+        $file   = $logdir . self::getFile('_trace_');
+        $data   = '';
+
+        if ($type != C::TYPE_TRACE) {
+            
+            $data  = self::getDataToWrite($type, $log);
+            $file = $logdir . self::getFile();
         }
-        $exists = file_exists($file);
-        file_put_contents($file, $str, FILE_APPEND | FILE_TEXT);
-        //Se criou o arquivo agora
-        if (!$exists) {
-            @chmod($file, 0777);
-            @chown($file, "apache");
-            @chgrp($file, "brudam");
-            //Apagar arquivos com mais de 5 dias
-            foreach (new DirectoryIterator($logdir) as $fileInfo) {
-                if (!$fileInfo->isDot() && !$fileInfo->isDir() && time() - $fileInfo->getCTime() >= 432000) {//5 dias ( 5 * 24 * 60 * 60)
-                    unlink($fileInfo->getRealPath());
+
+        @file_put_contents($file, $data, FILE_APPEND | FILE_TEXT);
+
+        if (!file_exists($file)) {
+
+            @chmod($file, C::STORAGE_MOD);
+
+            foreach (new \DirectoryIterator($logdir) as $fileInfo) {
+
+                if (
+                    !$fileInfo->isDot() && !$fileInfo->isDir() &&
+                    time() - $fileInfo->getCTime() >= C::CHANGE_TIME) {
+
+                    @unlink($fileInfo->getRealPath());
                 }
             }
         }
     }
 
-    /**
-     * Devolver tempo de excução do script, até o momento da chamada
-     */
-    public static function execTime() {
-        $mtime = explode(" ", microtime());
-        $endtime = $mtime[1] + $mtime[0];
-        return ($endtime - STARTTIME);
+    private static function getFile(): string
+    {
+        $p = trim(static::getPrefixToFile());
+        $d = date('Y-m-d');
+        $e = '.txt';
+
+        return $p . $d . $e;
+    }
+
+    private static function get($type, $log): string
+    {
+        $d = date('Y-m-d H:i:s');
+        $s = '%s %s: %s';
+
+       return sprintf($s, $d, $type, $log);
+    }
+
+    private static function getDataToWrite($type, $log): string
+    {
+        if (is_array($log) || is_object($log)) {
+            return self::get($type, print_r($log, true));
+        }
+
+        if (self::isException($log)) {
+            return self::get($type, $log->getTraceAsString());
+        }
+
+        if (is_string($log)) {
+            return self::get($type, $log . PHP_EOL);
+        }
+
+        return self::get($type, self::getVarDump($log));
+    }
+
+    private static function isException($log)
+    {
+        return (
+            is_object($log) && (
+                (get_class($log) == "Exception") ||
+                is_subclass_of($log, "Exception")
+            ));
+    }
+
+    private static function getVarDump($log)
+    {
+        ob_start();
+        var_dump($log);
+
+        return ob_get_clean();
     }
 
 }
+
