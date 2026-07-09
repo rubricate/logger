d5c8c2b317236bc4bb4fe39acdce4d3b77bf6b07
Estefanio N Santos <estefanions@gmail.com>
2026-07-09 20:02:15 -0300

    [#2607091930] refactor(logger): decouple constants and allow dynamic file expiration
    - Remove legacy ConstLogger class to avoid rigid configuration coupling.
    - Introduce static properties $changeTime and $storageMod in AbstractLogger.
    - Use Late Static Binding (static::) to let concrete classes override configurations.
    - Maintain broad compatibility down to PHP 8.2 runtimes.
    
    Ref: #v4.1.0

--- Rubricate/Logger/AbstractLogger.php
+++ Rubricate/Logger/AbstractLogger.php
@@ -5,14 +5,21 @@ declare(strict_types=1);
 namespace Rubricate\Logger;
 
 use DirectoryIterator;
-use Rubricate\Logger\ConstLogger as C;
 use Throwable;
 
 ini_set('xdebug.overload_var_dump', '0');
 
 abstract class AbstractLogger
 {
+    protected const TYPE_INFO  = 'INFO';
+    protected const TYPE_ERROR = 'ERROR';
+    protected const TYPE_DEBUG = 'DEBUG';
+    protected const TYPE_TRACE = 'TRACE';
+
     protected static string $ex = '.txt';
+
+    protected static int $changeTime = 86400 * 5;
+    protected static int $storageMod = 0775;
 
     abstract protected static function getDirFullPath(): string;
     abstract protected static function getPrefixToFile(): string;
@@ -20,21 +27,21 @@ abstract class AbstractLogger
     public static function info(mixed ...$messages): void
     {
         foreach ($messages as $info) {
-            self::set(C::TYPE_INFO, $info);
+            self::set(self::TYPE_INFO, $info);
         }
     }
 
     public static function error(mixed ...$messages): void
     {
         foreach ($messages as $error) {
-            self::set(C::TYPE_ERROR, $error);
+            self::set(self::TYPE_ERROR, $error);
         }
     }
 
     public static function debug(mixed ...$messages): void
     {
         foreach ($messages as $debug) {
-            self::set(C::TYPE_DEBUG, $debug);
+            self::set(self::TYPE_DEBUG, $debug);
         }
     }
 
@@ -45,11 +52,10 @@ abstract class AbstractLogger
 
         foreach ($messages as $log) {
             $data = self::getDataToWrite($type, $log);
-            self::set(C::TYPE_TRACE, $data);
+            self::set(self::TYPE_TRACE, $data);
         }
 
         $trace = [];
-
         foreach (debug_backtrace() as $v) {
             if (isset($v['args'])) {
                 foreach ($v['args'] as &$arg) {
@@ -59,12 +65,11 @@ abstract class AbstractLogger
                 }
             }
 
-            $filtered = array_filter($v, static fn($key) => $key !== 'object', ARRAY_FILTER_USE_KEY);
-            $trace[] = $filtered;
+            $trace[] = array_filter($v, static fn($key) => $key !== 'object', ARRAY_FILTER_USE_KEY);
         }
 
         $data .= "Trace: " . print_r($trace, true);
-        self::set(C::TYPE_TRACE, $data);
+        self::set(self::TYPE_TRACE, $data);
     }
 
     private static function set(?string $type, mixed $log): void
@@ -73,7 +78,7 @@ abstract class AbstractLogger
         $file   = $logdir . self::getFile('_trace_');
         $data   = '';
 
-        if ($type !== C::TYPE_TRACE) {
+        if ($type !== self::TYPE_TRACE) {
             $data = self::getDataToWrite($type, $log);
             $file = $logdir . self::getFile();
         }
@@ -81,12 +86,14 @@ abstract class AbstractLogger
         @file_put_contents($file, $data, FILE_APPEND | FILE_TEXT);
 
         if (!file_exists($file)) {
-            @chmod($file, C::STORAGE_MOD);
+            @chmod($file, static::$storageMod);
 
-            foreach (new DirectoryIterator($logdir) as $fileInfo) {
-                if (!$fileInfo->isDot() && !$fileInfo->isDir() &&
-                    (time() - $fileInfo->getCTime() >= C::CHANGE_TIME)) {
-                    @unlink($fileInfo->getRealPath());
+            if (is_dir($logdir)) {
+                foreach (new DirectoryIterator($logdir) as $fileInfo) {
+                    if (!$fileInfo->isDot() && !$fileInfo->isDir() && 
+                        (time() - $fileInfo->getCTime() >= static::$changeTime)) {
+                        @unlink($fileInfo->getRealPath());
+                    }
                 }
             }
         }
@@ -94,31 +101,27 @@ abstract class AbstractLogger
 
     private static function getFile(string $suffix = ''): string
     {
-        $p = trim(static::getPrefixToFile());
-        $d = date('Y-m-d');
-
-        return $p . $suffix . $d . self::$ex;
+        return trim(static::getPrefixToFile()) . $suffix . date('Y-m-d') . self::$ex;
     }
 
     private static function get(?string $type, string $log): string
     {
-        $d = date('Y-m-d H:i:s');
-        return sprintf('%s %s: %s', $d, $type ?? '', $log);
+        return sprintf('%s %s: %s', date('Y-m-d H:i:s'), $type ?? '', $log);
     }
 
     private static function getDataToWrite(?string $type, mixed $log): string
     {
         return match (true) {
-            is_array($log) || is_object($log) && !($log instanceof Throwable)
+            is_array($log) || is_object($log) && !($log instanceof Throwable) 
                 => self::get($type, print_r($log, true)),
 
-            $log instanceof Throwable
+            $log instanceof Throwable 
                 => self::get($type, $log->getTraceAsString()),
 
-            is_string($log)
+            is_string($log) 
                 => self::get($type, $log . PHP_EOL),
 
-            default
+            default 
                 => self::get($type, self::getVarDump($log)),
         };
     }
@@ -130,4 +133,3 @@ abstract class AbstractLogger
         return (string) ob_get_clean();
     }
 }
-
